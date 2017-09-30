7260dc2de9762cb59bb2b7aaa8614ce57985c4e6
syn52 <30964078+syn52@users.noreply.github.com>
2017-09-30 08:41:03 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,32 @@ import java.util.Scanner;
  * Exercises on if, for and while loops
  */
 public class Main {
-    public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);  // Look up the documentation for Scanner and what methods are available
-
-
-        //TODO: Write a Java program to compare two numbers.
-        int number1; // first number to compare
-        int number2; // second number to compare
+    // Look up the documentation for Scanner and what methods are available
+    Scanner input = new Scanner(System.in);
+    int number1; // first number to compare
+    int number2; // second number to compare
 
 
-
-
-        // TODO: Write a Java program that reads a floating-point number and prints "zero" if the number is zero.
-        // TODO: Otherwise, print "positive" or "negative".
+    //TODO: Write a Java program to compare two numbers.
 
 
 
-        // TODO: Write a program in Java to display the first 10 natural numbers.
+
+
+    // TODO: Write a Java program that reads a floating-point number and prints "zero" if the number is zero.
+    // TODO: Otherwise, print "positive" or "negative".
 
 
 
-        // TODO: Write a Java program to reverse a string. Print the before and after.
 
-    }
+
+    // TODO: Write a program in Java to display the first 10 natural numbers.
+
+
+
+
+
+
+    // TODO: Write a Java program to reverse a string. Print the before and after.
 }
